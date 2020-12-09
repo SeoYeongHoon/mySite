2b68c4989d45f46b7f17517e45e03e3e2eb85564
SeoYeongHoon <tjdudgns1996@naver.com>
2020-12-09 13:03:11 +0900

    MySite

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,13 @@
+export const homeObjOne = {
+    lightBg: false,
+    primary: true,
+    lightBg: false, 
+    imgStart: '', 
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    buttonLabel: 'Get Started', 
+    description: 'This is Test!!!!!!!!', 
+    headline: 'This is Test too~', 
+    lightText: true, 
+    topLine: 'Maple Story'
+};
